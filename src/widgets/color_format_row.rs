e1d58c410a58d23ef4a50de9555732da616fe649
FineFindus <63370021+FineFindus@users.noreply.github.com>
2023-03-08 20:28:14 +0100

    feat(color-entry): add apply button
    Closes #45. Changes the  button to a suggested apply button when editing the entry.

--- src/widgets/color_format_row.rs
+++ src/widgets/color_format_row.rs
@@ -123,6 +123,28 @@ impl ColorFormatRow {
         self.set_property("color", &text);
     }
 
+    /// Switches the button next to the entry.
+    ///
+    /// If `show_apply` is set to true, the button will be an apply button with a checkmark icon
+    /// and the `suggested-action` color.
+    /// If set to false, the button will show the normal copy symbol.
+    pub fn switch_button(&self, show_apply: bool) {
+        let button = &self.imp().format_button;
+        if show_apply {
+            button.set_icon_name("checkmark-symbolic");
+            button.add_css_class("suggested-action");
+        } else {
+            button.set_icon_name("edit-copy-symbolic");
+            button.remove_css_class("suggested-action");
+        }
+    }
+
+    /// Checks if entry has been edited and is not empty.
+    fn text_changed(&self) -> bool {
+        let entry_text = self.imp().entry.buffer().text();
+        !entry_text.trim().is_empty() && entry_text.trim() != self.color().trim()
+    }
+
     /// Indicate an error with the input occurred by applying the libadwaita error style class
     /// for a short time (250ms), so the entry shows the error for a short moment.
     pub fn show_error(&self) {
@@ -159,7 +181,7 @@ impl ColorFormatRow {
 
         //bind texts
         self.bind_property("color", &*self.imp().entry, "text")
-            .flags(glib::BindingFlags::SYNC_CREATE | glib::BindingFlags::BIDIRECTIONAL)
+            .flags(glib::BindingFlags::SYNC_CREATE)
             .build();
         //bind editable
         self.bind_property("editable", &*self.imp().entry, "editable")
@@ -171,6 +193,12 @@ impl ColorFormatRow {
         self.bind_property("editable", &*self.imp().entry, "can-target")
             .flags(glib::BindingFlags::SYNC_CREATE)
             .build();
+
+        self.imp()
+            .entry
+            .connect_changed(glib::clone!(@weak self as widget => move |_entry| {
+                widget.switch_button(widget.text_changed());
+            }));
     }
 
     /// Registers a signal for when the text entry is changed to emit
@@ -181,6 +209,7 @@ impl ColorFormatRow {
             .connect_activate(glib::clone!(@weak self as format_row => move |entry| {
                 let text = entry.buffer().text();
                 if format_row.is_visible() && !text.is_empty() {
+                    format_row.switch_button(false);
                     format_row.emit_by_name("text-edited", &[&text.to_value()])
                 }
             }));
@@ -197,15 +226,24 @@ impl ColorFormatRow {
             .build();
     }
 
-    /// Copy the text to the users clipboard and a signal.
+    /// Callback when the button next to the entry is pressed.
     ///
-    /// This is bound as the callback when the copy-icon-button is pressed.
+    /// Depending on, if the entry has been edited, the button will either
+    /// - apply the edited entry by emitting a activate signal
+    /// - or copy the text to the users clipboard and emit the `copied-text` signal
     #[template_callback]
-    fn on_copy_pressed(&self, _: &gtk::Button) {
-        let text = self.imp().entry.text().to_string();
-        log::debug!("Copied text: {text}");
-        let clipboard = self.clipboard();
-        clipboard.set_text(&text);
-        self.emit_by_name("copied-text", &[&text.to_value()])
+    fn on_button_pressed(&self, _button: &gtk::Button) {
+        let text = self.imp().entry.buffer().text();
+        if self.text_changed() {
+            log::debug!("Applied entry content: {}", text);
+            self.switch_button(false);
+            self.imp().entry.emit_activate();
+            return;
+        } else {
+            log::debug!("Copied text: {text}");
+            let clipboard = self.clipboard();
+            clipboard.set_text(&text);
+            self.emit_by_name("copied-text", &[&text.to_value()])
+        }
     }
 }
