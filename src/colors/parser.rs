f81fc1a0594ebaaa71494f049ad21ab3c676bffe
FineFindus <63370021+FineFindus@users.noreply.github.com>
2023-03-14 21:53:11 +0100

    test(hex-parser): add tests for hex parsing

--- src/colors/parser.rs
+++ src/colors/parser.rs
@@ -45,3 +45,50 @@ pub fn hex_color(input: &str, alpha_position: AlphaPosition) -> IResult<&str, Co
 
     Ok((input, Color::rgba(red, green, blue, alpha)))
 }
+
+#[cfg(test)]
+mod parse_hex {
+    use super::*;
+
+    #[test]
+    fn it_parse_hex_without_alpha() {
+        assert_eq!(
+            Color::rgb(46, 52, 64),
+            hex_color("2e3440", AlphaPosition::None).unwrap().1
+        );
+        assert_eq!(
+            Color::rgb(46, 52, 64),
+            hex_color("#2e3440", AlphaPosition::None).unwrap().1
+        );
+    }
+    #[test]
+    fn it_parse_hex_with_alpha_start() {
+        assert_eq!(
+            Color::rgba(46, 52, 64, 40),
+            hex_color("282e3440", AlphaPosition::Start).unwrap().1
+        );
+        assert_eq!(
+            Color::rgb(46, 52, 64),
+            hex_color("#2e3440", AlphaPosition::None).unwrap().1
+        );
+    }
+
+    #[test]
+    fn it_parse_hex_with_alpha_end() {
+        assert_eq!(
+            Color::rgba(46, 52, 64, 40),
+            hex_color("2e344028", AlphaPosition::End).unwrap().1
+        );
+        assert_eq!(
+            Color::rgba(46, 52, 64, 40),
+            hex_color("#2e344028", AlphaPosition::End).unwrap().1
+        );
+    }
+    #[test]
+    fn it_fails_input_over_maximum() {
+        //longer than 8 chase
+        assert!(hex_color("#this is not a color", AlphaPosition::None).is_err());
+        //longer than 6 chars
+        assert!(hex_color("#1234567", AlphaPosition::None).is_err());
+    }
+}
