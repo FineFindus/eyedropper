bc048086ed8269cf243a4309abe8834f69b0ea28
FineFindus <63370021+FineFindus@users.noreply.github.com>
2023-04-01 09:59:35 +0200

    fix(search): use gdk::RGBA to parse color in all places

--- src/application.rs
+++ src/application.rs
@@ -265,7 +265,7 @@ impl App {
 impl SearchProviderImpl for App {
     fn activate_result(&self, identifier: ResultID, _terms: &[String], timestamp: u32) {
         let window = self.main_window();
-        window.set_color(Color::from(identifier));
+        window.set_color(gdk::RGBA::parse(identifier).unwrap().into());
         window.present_with_time(timestamp);
     }
 
