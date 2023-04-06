68f476c32b0dd9aa2aafc415c00369cb923993c1
FineFindus <63370021+FineFindus@users.noreply.github.com>
2023-04-06 22:53:12 +0200

    feat(parser): parse rgb

--- src/colors/parser.rs
+++ src/colors/parser.rs
@@ -1,6 +1,9 @@
 use nom::{
     bytes::complete::{is_not, tag, take_while_m_n},
+    character::{complete::multispace0, is_digit, is_hex_digit},
     combinator::{map_res, opt},
+    error::ParseError,
+    multi::separated_list0,
     sequence::{delimited, Tuple},
     IResult,
 };
@@ -11,12 +14,28 @@ fn from_hex(input: &str) -> Result<u8, std::num::ParseIntError> {
     u8::from_str_radix(input, 16)
 }
 
-fn is_hex_digit(c: char) -> bool {
-    c.is_ascii_hexdigit()
+fn from_int(input: &str) -> Result<u8, std::num::ParseIntError> {
+    input.parse::<u8>()
 }
 
 fn hex_primary(input: &str) -> IResult<&str, u8> {
-    map_res(take_while_m_n(2, 2, is_hex_digit), from_hex)(input)
+    map_res(
+        take_while_m_n(2, 2, |char| is_hex_digit(char as u8)),
+        from_hex,
+    )(input)
+}
+
+fn ws<'a, F: 'a, O, E: ParseError<&'a str>>(
+    inner: F,
+) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+where
+    F: Fn(&'a str) -> IResult<&'a str, O, E>,
+{
+    delimited(multispace0, inner, multispace0)
+}
+
+fn parse_int(input: &str) -> IResult<&str, u8> {
+    map_res(take_while_m_n(1, 3, |char| is_digit(char as u8)), from_int)(input)
 }
 
 pub fn hex_color(input: &str, alpha_position: AlphaPosition) -> IResult<&str, Color> {
@@ -91,4 +110,12 @@ mod parse_hex {
         //longer than 6 chars
         assert!(hex_color("#1234567", AlphaPosition::None).is_err());
     }
+}
+
+pub fn rgb(input: &str) -> IResult<&str, Color> {
+    let (_input, values) = delimited(ws(tag("rgb(")), is_not(")"), tag(")"))(input)?;
+
+    let (input, color) = separated_list0(ws(tag(",")), parse_int)(values)?;
+    log::debug!("Color: {:?}", color);
+    Ok((input, Color::random()))
 }
