da2c733b5b466a8965411166b62ad0fca6a52cb2
FineFindus <FineFindus@proton.me>
2023-05-20 21:44:09 +0200

    feat(parser): add lms parser

--- src/window.rs
+++ src/window.rs
@@ -911,6 +911,28 @@ impl AppWindow {
             }),
         );
 
+        imp.lms_row.connect_closure(
+            "text-edited",
+            false,
+            glib::closure_local!(@watch self as window => move |format_row: ColorFormatRow, color: String| {
+                log::debug!("Changed LMS entry: {color}");
+
+                if let Some(current_color) = window.color(){
+                    match Color::from_lms_string(&color) {
+                        Ok(color) if color != current_color => {
+                            window.set_color(color);
+                            format_row.show_success();
+                        },
+                        Err(_) => {
+                            log::debug!("Failed to parse color: {color}");
+                            format_row.show_error();
+                        },
+                        _ => {}
+                    }
+                }
+            }),
+        );
+
         imp.hunter_lab_row.connect_closure(
             "text-edited",
             false,
