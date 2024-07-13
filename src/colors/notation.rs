1c8d2ae1b6ce5f4ccbdefc0ff4effac0ee5d5f46
FineFindus <FineFindus@proton.me>
2024-07-13 12:59:13 +0200

    refactor: dynamically build format rows
    Also fixes an issue, where the default window size would be reset to the
    largest size upon startup.

--- src/colors/notation.rs
+++ src/colors/notation.rs
@@ -1,6 +1,11 @@
-use gtk::{gio, prelude::SettingsExt};
+use std::str::FromStr;
 
-use crate::config;
+use gtk::{
+    gio,
+    prelude::{SettingsExt, WidgetExt},
+};
+
+use crate::{config, widgets::color_format_row::ColorFormatRow};
 
 use super::{
     color::{Color, ColorError},
@@ -83,5 +88,57 @@ impl Notation {
             Notation::Name => color_names::name(color, true, true, true, true)
                 .unwrap_or(gettextrs::gettext("Not named")),
         }
+    }
+
+    pub fn widget(&self) -> ColorFormatRow {
+        ColorFormatRow::new(self)
+    }
+
+    pub fn display_copy_string(&self) -> String {
+        gettextrs::gettext(match self {
+            Notation::Hex => "Copy Hex Code",
+            Notation::Rgb => "Copy RGB",
+            Notation::Hsl => "Copy HSL",
+            Notation::Hsv => "Copy HSV",
+            Notation::Cmyk => "Copy CMYK",
+            Notation::Xyz => "Copy Xyz",
+            Notation::Lab => "Copy CIELAB",
+            Notation::Hwb => "Copy HWB",
+            Notation::Hcl => "Copy CIELCh / HCL",
+            Notation::Lms => "Copy LMS",
+            Notation::HunterLab => "Copy Hunter Lab",
+            Notation::Oklab => "Copy Oklab",
+            Notation::Oklch => "Copy Oklch",
+            Notation::Name => "Copy Name",
+        })
+    }
+}
+
+impl FromStr for Notation {
+    type Err = ColorError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s.to_lowercase().trim() {
+            "hex" => Self::Hex,
+            "rgb" => Self::Rgb,
+            "hsl" => Self::Hsl,
+            "hsv" => Self::Hsv,
+            "cmyk" => Self::Cmyk,
+            "xyz" => Self::Xyz,
+            "cielab" => Self::Lab,
+            "hwb" => Self::Hwb,
+            "hcl" => Self::Hcl,
+            "name" => Self::Name,
+            "lms" => Self::Lms,
+            "hunterlab" => Self::HunterLab,
+            "oklab" => Self::Oklab,
+            "oklch" => Self::Oklch,
+            _ => {
+                log::error!("Failed to parse notation: {}", s);
+                return Err(ColorError::ParsingError(
+                    "Failed to get color notation".to_owned(),
+                ));
+            }
+        })
     }
 }
